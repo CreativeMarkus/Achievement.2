72cadaefc416fbd643c300443c058a288489fa77
CreativeMarkus <mark@creativemarkus.com>
2026-02-13 13:15:23 -0500

    Add AWS S3 support for persistent image storage on Heroku

--- A2_Recipe_App/src/recipe_project/urls.py
+++ A2_Recipe_App/src/recipe_project/urls.py
@@ -2,8 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.static import serve
-from django.urls import re_path
 from .views import login_view, logout_view
 
 
@@ -20,11 +18,6 @@ urlpatterns = [
     path('logout-success/', logout_success_view, name='logout-success'),
 ]
 
-# Serve media files in development
+# Serve media files in development only
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-else:
-    # In production, serve media files through the static files path
-    urlpatterns += [
-        re_path(r'^static/recipes/images/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-    ]
