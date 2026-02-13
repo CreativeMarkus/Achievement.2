abc17497a0e102854807593325adb74100d30bd4
CreativeMarkus <mark@creativemarkus.com>
2026-02-13 13:12:57 -0500

    Fix: Serve media files in production (Heroku) using Django serve view

--- A2_Recipe_App/src/recipe_project/urls.py
+++ A2_Recipe_App/src/recipe_project/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import re_path
 from .views import login_view, logout_view
 
 
@@ -17,4 +19,12 @@ urlpatterns = [
     path('logout/', logout_view, name='logout'),
     path('logout-success/', logout_success_view, name='logout-success'),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Serve media files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    # In production, serve media files through the static files path
+    urlpatterns += [
+        re_path(r'^static/recipes/images/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
